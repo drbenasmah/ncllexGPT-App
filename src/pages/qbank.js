fcca3b09d8e85ef6b8f5cfeb09249abe2527e87a
drbenasmah <drbenasmah2016@gmail.com>
2023-12-21 23:12:55 +0100

    netlify

---
+++ src/pages/qbank.js
@@ -0,0 +1,15 @@
+function QuestionBank() {
+  return (
+    <>
+      <h1>Under construction</h1>
+
+      <p>
+        <Link to="./" relative="path">
+          Back
+        </Link>
+      </p>
+    </>
+  );
+}
+
+export default QuestionBank;
